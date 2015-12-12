76c3aaec7f8186b46e8ed20d9a8564b621f32df8
Ian Atha <ian@atha.io>
2015-12-12 04:15:27 -0800

    support for mapping many labels with a common prefix.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -38,23 +38,23 @@ class HomeController < ApplicationController
     from_gmail = gmail_service_from_account_link_id(params[:from_account_id])
     to_gmail = gmail_service_from_account_link_id(params[:to_account_id])
 
-    from_sync_labels = from_gmail.list_user_labels('me').labels.select { |x| x.name.start_with? "mb/" }
-    from_sync_label_names = from_sync_labels.map { |x| x.id }
-    if not from_sync_label_ids
+    from_sync_labels = from_gmail.list_user_labels('me').labels.select { |x| x.name.start_with? "MB/" }
+    from_sync_label_names = from_sync_labels.map { |x| x.name }
+    if not from_sync_label_names
       raise "Couldn't find label"
     end
 
-    to_sync_labels = to_gmail.list_user_labels('me').labels.select { |x| from_sync_label_names.has?(x.name) }
-    to_sync_label_names = to_sync_labels.map { |x| x.id }
+    to_sync_labels = to_gmail.list_user_labels('me').labels.select { |x| from_sync_label_names.include?(x.name) }
+    to_sync_label_names = to_sync_labels.map { |x| x.name }
 
     label_mappings = {}
 
-    from_sync_label_names.each do |label| 
-      if not to_sync_label_names.has?(label)
-        new_label = to_gmail.create_user_label('me', Google::Apis::GmailV1::Label.new(name: label))
-        label_mappings[label] = new_label.id
+    from_sync_labels.each do |label| 
+      if not to_sync_label_names.include?(label.name)
+        new_label = to_gmail.create_user_label('me', Google::Apis::GmailV1::Label.new(name: label.name))
+        label_mappings[label.id] = new_label.id
       else 
-        label_mappings[label] = to_sync_labels.select { |x| x.name == label }.first.id
+        label_mappings[label.id] = to_sync_labels.select { |x| x.name == label.name }.first.id
       end
     end
 
@@ -64,8 +64,9 @@ class HomeController < ApplicationController
     from_gmail.batch do |from_gmail|
       message_ids.each do |message_id|
         from_gmail.get_user_message('me', message_id.id, format: "RAW") do |message, err|
+          puts message
           transcribed_message = Google::Apis::GmailV1::Message.new(raw: message.raw)
-          transcribed_message.label_ids = [label_mappings.first]
+          transcribed_message.label_ids = message.label_ids.map { |label_id| label_mappings[label_id] }
           transcribed_message.thread_id = thread_mapping[message.thread_id] if thread_mapping[message.thread_id]
           inserted_message = to_gmail.insert_user_message('me', transcribed_message, internal_date_source: "dateHeader", deleted: false)
           thread_mapping[message.thread_id] = inserted_message.thread_id
