01de2426b30fc4cd094a199b8f766cd9fa1890b6
Ian Atha <ian@atha.io>
2015-12-12 13:45:30 -0800

    Remember mapped messages, and do not remap them.

--- app/controllers/sync_controller.rb
+++ app/controllers/sync_controller.rb
@@ -5,8 +5,7 @@ require 'mail'
 Google::Apis.logger.level = Logger::WARN
 
 class SyncController < ApplicationController
-  def gmail_service_from_account_link_id(account_link_id)
-    account_link = AccountLink.find(account_link_id)
+  def gmail_service_from_account_link(account_link)
     gmail = Google::Apis::GmailV1::GmailService.new
     oauth_client = Signet::OAuth2::Client.new()
     oauth_client.access_token = account_link.credentials[:token]
@@ -19,12 +18,15 @@ class SyncController < ApplicationController
     return email.header['Message-Id']
   end
 
+  # GmailServive documentation: https://github.com/google/google-api-ruby-client/blob/master/generated/google/apis/gmail_v1/service.rb
   def insert_email_in_user()
     thread_mapping = {}
 
-    # https://github.com/google/google-api-ruby-client/blob/master/generated/google/apis/gmail_v1/service.rb
-    from_gmail = gmail_service_from_account_link_id(params[:from_account_id])
-    to_gmail = gmail_service_from_account_link_id(params[:to_account_id])
+    from_account_link = current_user.account_link.find(params[:from_account_id])
+    to_account_link = current_user.account_link.find(params[:to_account_id])
+
+    from_gmail = gmail_service_from_account_link(from_account_link)
+    to_gmail = gmail_service_from_account_link(to_account_link)
 
     from_sync_labels = from_gmail.list_user_labels('me').labels.select { |x| x.name.start_with? "MB/" }
     from_sync_label_names = from_sync_labels.map { |x| x.name }
@@ -58,14 +60,30 @@ class SyncController < ApplicationController
     from_gmail.batch do |from_gmail|
       message_ids.each do |message_id|
         from_gmail.get_user_message('me', message_id.id, format: "RAW") do |message, err|
-          message_id = get_message_id(message.raw)
+          raise err if err
 
-          transcribed_message = Google::Apis::GmailV1::Message.new(raw: message.raw)
-          transcribed_message.label_ids = message.label_ids.map { |label_id| label_mappings[label_id] } + extra_labels
-          transcribed_message.thread_id = thread_mapping[message.thread_id] if thread_mapping[message.thread_id]
-          inserted_message = to_gmail.insert_user_message('me', transcribed_message, internal_date_source: "dateHeader", deleted: false)
-          thread_mapping[message.thread_id] = inserted_message.thread_id
-          message_count = message_count + 1
+          email_message_id = get_message_id(message.raw).value
+
+          # have we seen this message before?
+          from_message_mapping = from_account_link.message_id_mapping.where(email_message_id: email_message_id).first_or_create do |message_mapping|
+            message_mapping.provider_message_id = message_id.id
+            message_mapping.provider_thread_id = message_id.thread_id
+          end
+
+          to_message_mapping = to_account_link.message_id_mapping.where(email_message_id: email_message_id).first_or_create do |message_mapping|
+            # if we're in here, it means the message hasn't been 'mapped' before
+
+            transcribed_message = Google::Apis::GmailV1::Message.new(raw: message.raw)
+            transcribed_message.label_ids = message.label_ids.map { |label_id| label_mappings[label_id] } + extra_labels
+            transcribed_message.thread_id = thread_mapping[message.thread_id] if thread_mapping[message.thread_id]
+
+            inserted_message = to_gmail.insert_user_message('me', transcribed_message, internal_date_source: "dateHeader", deleted: false)
+            thread_mapping[message.thread_id] = inserted_message.thread_id
+            message_count = message_count + 1
+
+            message_mapping.provider_message_id = inserted_message.id
+            message_mapping.provider_thread_id = inserted_message.thread_id
+          end
         end
       end
     end
--- app/models/account_link.rb
+++ app/models/account_link.rb
@@ -1,3 +1,4 @@
 class AccountLink < ActiveRecord::Base
 	store :credentials, coder: JSON
+	has_many :message_id_mapping
 end
