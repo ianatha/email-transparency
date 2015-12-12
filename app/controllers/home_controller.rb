ef8aae33a88cb169c8630bdd30569917ab073029
Ian Atha <ian@atha.io>
2015-12-12 13:19:31 -0800

    Update AccountLink instead of recreating it.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,16 +6,14 @@ class HomeController < ApplicationController
 
   def link_account
     auth = request.env["omniauth.auth"]
-    other_account_links = current_user.account_link.where(provider: auth[:provider], username: auth[:info][:email]).to_a
-    if other_account_links.length
-      other_account_links.each do |a|
-        a.delete
-      end
+    account_link = current_user.account_link.where(provider: auth[:provider], username: auth[:info][:email]).first
+    if account_link
       flash[:notice] = "updated existing link with #{auth[:provider]}"
     else
+      account_link = current_user.account_link.new(provider: auth[:provider], username: auth[:info][:email])
       flash[:notice] = "inserted new link with #{auth[:provider]}"
     end
-    account_link = current_user.account_link.create(provider: auth[:provider], username: auth[:info][:email], credentials: auth[:credentials])
+    account_link.update(credentials: auth[:credentials])
     redirect_to "/home"
   end
 end
